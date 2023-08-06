e9333fb60a7d715e14c608bfa2dc1e626ef04f94
Sugyan09 <141542370+Sugyan09@users.noreply.github.com>
2023-08-06 12:00:16 +0545

    chesshoook website

---
+++ contactus.js
@@ -0,0 +1,4 @@
+document.querySelector('.header-button').addEventListener('click', function() {
+    var targetUrl = "signup.html";
+    window.location.href = targetUrl;
+    });
---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelector('.header-button' ).addEventListener('click', function() {
+var targetUrl = "signup.html";
+window.location.href = targetUrl;
+});
+
+document.querySelector('.aboutanditem-button').addEventListener('click', function() {
+
+var targetUrl = "signup.html";
+window.location.href = targetUrl;
+});
